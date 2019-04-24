57d5a861d3b9fa70fc277e4d5db2d8179cea539b
Piotr Skorupski <piotrsko@microsoft.com>
2019-04-24 12:24:06 +0200

    Added BABEL to compile JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-    entry: './src/bug.js',
+    entry: './src/bug.jsx',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
     output: {
       path: __dirname + '/dist',
       publicPath: '/',
