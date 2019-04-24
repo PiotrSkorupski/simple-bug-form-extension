5e6c82b1283f87552dbdefdb7c9291510fe72c5f
Piotr Skorupski <piotrsko@microsoft.com>
2019-04-24 09:41:51 +0200

    First commit

---
+++ overview.md
@@ -0,0 +1,1 @@
+Piotrsko test
---
+++ src/components/hello.js
@@ -0,0 +1,9 @@
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>React component</div>
+        );
+    }
+}
+
+export default Hello;
