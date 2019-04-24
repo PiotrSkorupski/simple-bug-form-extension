87b7bd89de5700b049dd5f9f4d2d4ec8b6862b25
Piotr Skorupski <piotrsko@microsoft.com>
2019-04-24 11:38:29 +0200

    Added webpack

--- src/bug.js
+++ src/bug.js
@@ -3,6 +3,7 @@ class Greetings extends React.Component
     render()
     {
         return React.createElement('h1', null, 'React, ' + this.props.name + '!');
+        //return <h1>H1 with JSX</h1>;
     }
 }
 
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './src/bug.js',
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bug.js'
+    }
+  };
