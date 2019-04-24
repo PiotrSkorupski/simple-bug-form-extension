5cee0c0b17b46c7f9dbb065c39042983e413283d
Piotr Skorupski <piotrsko@microsoft.com>
2019-04-24 15:29:25 +0200

    Added tabs form DevOps UI Library

--- src/bug.jsx
+++ src/bug.jsx
@@ -18,16 +18,39 @@ class Greetings extends React.Component
 }
 
 class SimpleBugFormHubContent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {selectedTabId: "my-open-bugs"};
+    }
+
     render() {
         return (
-            <Page className="sample-hub flex-grow">
-            <Header title="Sample Hub"/>
+            <Page className="flex-grow">
+            <Header title="Report a bug" titleSize={TitleSize.Large}/>
+                <TabBar tabSize={TabSize.Tall} selectedTabId={this.state.selectedTabId}>
+                    <Tab name="My Open Bugs" id="my-open-bugs" />
+                    <Tab name="All My Bugs" id="my-all-bugs" />
+                </TabBar>
+                <div className="page-content">
+                    { this.getPageContent() }
+                </div>
             </Page>
         );
     }
+
+    getPageContent() {
+        const { selectedTabId } = this.state;
+        
+        if (selectedTabId === "my-open-bugs")
+            return "My open bugs content";
+        if (selectedTabId === "my-all-bugs")
+            return "All my bugs content";
+    }
 }
 
+
 ReactDOM.render(
-    React.createElement(SimpleBugFormHubContent, { name : 'Piotrsko' }),
+    React.createElement(SimpleBugFormHubContent, { name : 'Report a Bug' }),
     document.getElementById('report-bug-root')
     );
