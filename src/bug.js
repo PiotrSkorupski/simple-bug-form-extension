3315ea17a58f4a0138f584a5e4cee0c3b14e4988
Piotr Skorupski <piotrsko@microsoft.com>
2019-04-24 09:56:41 +0200

    Moved react code from HTML to JS without import

--- src/bug.js
+++ src/bug.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import Hello from './components/Hello';
-
-//ReactDOM.render(<Hello/>, document.getElementById('report-bug-root'));
-
-window.onload = function()
+class Greetings extends React.Component
 {
-    class Greetings extends React.Component
+    render()
     {
-        render()
-        {
-            return React.createElement('h1', null, 'React, ' + this.props.name + '!');
-        }
+        return React.createElement('h1', null, 'React, ' + this.props.name + '!');
     }
-    ReactDOM.render(
-        React.createElement(Greetings, { name : 'Piotrsko' }),
-        document.getElementById('report-bug-root')
+}
+
+ReactDOM.render(
+    React.createElement(Greetings, { name : 'Piotrsko' }),
+    document.getElementById('report-bug-root')
     );
-};
